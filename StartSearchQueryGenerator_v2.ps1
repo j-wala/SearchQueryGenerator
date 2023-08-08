8cac5717fda1fa09839af871d71ddb32f3945aea
Jens Wala <jens@j-wala.de>
2023-08-08 15:19:40 +0200

    Added powershell script for easy start of venv and generator

---
+++ StartSearchQueryGenerator_v2.ps1
@@ -0,0 +1,28 @@
+# Get the directory of the script being executed
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Specify the relative paths to your Python executable and virtual environment
+$pythonExecutable = Join-Path $scriptDirectory ".venv\Scripts\python.exe"
+$virtualEnvPath = Join-Path $scriptDirectory ".venv"
+
+# Activate the virtual environment
+$activateScript = Join-Path $virtualEnvPath "Scripts\Activate"
+if (Test-Path $activateScript) {
+    Write-Host "Activating virtual environment..."
+    & $activateScript
+} else {
+    Write-Host "Virtual environment activation script not found."
+}
+
+# Run the main.py file using the activated Python environment
+if (Test-Path $pythonExecutable) {
+    $mainScript = Join-Path $scriptDirectory "SearchQuerygeneratorWithUi_ChatGPT_v8.py"
+    if (Test-Path $mainScript) {
+        Write-Host "Running Search Query Generator..."
+        & $pythonExecutable $mainScript
+    } else {
+        Write-Host "SearchQuerygeneratorWithUi_ChatGPT_v8.py file not found."
+    }
+} else {
+    Write-Host "Python executable not found."
+}
